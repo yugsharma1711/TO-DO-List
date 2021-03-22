e5dff9f2fd9d391f0d266cf2ccf7545d8568e1e9
yugsharma1711 <imperialyug0805@gmail.com>
2021-03-22 10:42:07 +0530

    Added a Source File

---
+++ source.cpp
@@ -0,0 +1,26 @@
+//Source file for making a Todo list 
+
+
+#include<iostream>
+
+using namespace std;
+class Node{
+public:
+    string content;
+    Node *next;
+};
+int main(){
+    Node *head = new Node();
+    return 0 ;
+}
+
+void push(Node*head_ref){
+    while(head_ref->next != NULL){
+        head_ref = head_ref->next;
+    }
+    Node *new_node = new Node();
+    head_ref->next = new_node;
+    new_node->next = NULL;
+    cout<<"Enter the task to be added \n";
+    cin>>new_node->content;
+}
